d6a1a574004a76e6a2cce63998ae85b8890c1ed9
Ayrton Iseri <ayrtoniseri@hotmail.com>
2024-08-08 17:17:43 -0300

    terraform fmt command ran for all previous development

--- infra/app/aws_ec2.tf
+++ infra/app/aws_ec2.tf
@@ -1,20 +1,20 @@
 resource "aws_instance" "marmita_backend_webserver" {
-    ami = "ami-0ae8f15ae66fe8cda"
-    associate_public_ip_address = false
+  ami                         = "ami-0ae8f15ae66fe8cda"
+  associate_public_ip_address = false
 
-    iam_instance_profile = aws_iam_instance_profile.ssm_managed_ec2_instance_profile_core.name
-    instance_type = "t2.micro"
-    security_groups = [aws_security_group.webserver_backend_sg.id]
-    source_dest_check = false
-    subnet_id = module.vpc.private_subnets[0]
-    tenancy = "default"
+  iam_instance_profile = aws_iam_instance_profile.ssm_managed_ec2_instance_profile_core.name
+  instance_type        = "t2.micro"
+  security_groups      = [aws_security_group.webserver_backend_sg.id]
+  source_dest_check    = false
+  subnet_id            = module.vpc.private_subnets[0]
+  tenancy              = "default"
 
-    tags = {
-        Name = "marmita-backend-webserver"
-    }
+  tags = {
+    Name = "marmita-backend-webserver"
+  }
 }
 
 resource "aws_iam_instance_profile" "ssm_managed_ec2_instance_profile_core" {
-    name = "SSMManagedEC2InstanceCore"
-    role = aws_iam_role.ec2_ssm_permission_role.name
+  name = "SSMManagedEC2InstanceCore"
+  role = aws_iam_role.ec2_ssm_permission_role.name
 }
--- infra/app/aws_iam.tf
+++ infra/app/aws_iam.tf
@@ -10,10 +10,10 @@ data "aws_iam_policy_document" "instance_assume_role_policy" {
 }
 
 resource "aws_iam_role" "ec2_ssm_permission_role" {
-    name = "SSMManagedEC2InstanceRole"
-    assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
+  name               = "SSMManagedEC2InstanceRole"
+  assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
 
-    managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
+  managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
 }
 
 
