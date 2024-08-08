dc8e63155a248f21c98e824a393d037afb726081
Ayrton Iseri <ayrtoniseri@hotmail.com>
2024-08-08 17:17:04 -0300

    Added an ec2 instance for the webserver, along with SG and an instance profile for ssm connecting into it

---
+++ infra/app/aws_iam.tf
@@ -0,0 +1,20 @@
+data "aws_iam_policy_document" "instance_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ec2_ssm_permission_role" {
+    name = "SSMManagedEC2InstanceRole"
+    assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
+
+    managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"]
+}
+
+
+
---
+++ infra/app/aws_sg.tf
@@ -0,0 +1,6 @@
+resource "aws_security_group" "webserver_backend_sg" {
+    name = "webserver-backend-sg"
+    description = "Security group of the webservers backend hosting ec2"
+    vpc_id = module.vpc.vpc_id
+}
+
