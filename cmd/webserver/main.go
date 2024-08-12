9417819291428db61b1d168864bcd6eefd9bbb6a
Ayrton Iseri <ayrtoniseri@hotmail.com>
2024-08-12 00:54:26 -0300

    Added new docker push logic to capture updates using dockerhub

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World! Testando aqui rapaziada")
+	fmt.Fprint(w, "Hello World! Testando aqui rapaziada nova versao")
 }
 
 func main() {
