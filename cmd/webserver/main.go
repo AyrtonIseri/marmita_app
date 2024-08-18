9fad4690d97cabe5bdc00dd829778b9418d051a9
Ayrton Iseri <ayrtoniseri@hotmail.com>
2024-08-18 15:20:34 -0300

    Added twilio whatsapp messaging to the webserver

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -3,13 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net/http/httputil"
+
+	"github.com/twilio/twilio-go/twiml"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World with CD!")
+
+	dump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("New request received. Information: \n %s \n", string(dump))
+
+	message := &twiml.MessagingMessage{
+		Body: "message received.",
+		To:   "mock_whatsapp",
+		From: "twilio_whatsapp",
+	}
+	twimlResult, err := twiml.Messages([]twiml.Element{message})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprint(w, twimlResult)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/whatsapp", handler)
 	http.ListenAndServe(":80", nil)
 }
