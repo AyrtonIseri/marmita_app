ff411a6fc01f4ccfde22fc10f08aca6c1f8d3635
Ayrton Iseri <ayrtoniseri@hotmail.com>
2024-08-21 22:37:44 -0300

    Implemented cookies handling logic

--- view/chatbot/whatsapp.go
+++ view/chatbot/whatsapp.go
@@ -3,9 +3,10 @@ package whatsapp
 import (
 	"fmt"
 	"marmita/env"
+	"marmita/internals/cookies"
 	"marmita/types"
 	"net/http"
-	"strings"
+	"strconv"
 
 	"github.com/twilio/twilio-go/twiml"
 )
@@ -26,11 +27,32 @@ func (cb Chatbot) MessageHandler(w http.ResponseWriter, r *http.Request) error {
 		return err
 	}
 
-	FinalBody := strings.Builder{}
-	FinalBody.WriteString("Message received. Submitting a copy: ")
-	FinalBody.WriteString(Request.Body)
+	cookieVal, err := cookies.ReadCookies(r, "stage")
+	if err != nil {
+		cookieVal = "1"
+	}
 
-	cb.WriteToUser(Request.WppUser, FinalBody.String(), w)
+	stage, err := strconv.Atoi(cookieVal)
+	if err != nil {
+		fmt.Println("Cookie sent is not a valid integer:", cookieVal)
+		stage = 1
+	}
+
+	FinalBody := "Message received. Submitting a copy: " + Request.Body + "\nYour current stage is: " + strconv.Itoa(stage)
+
+	stage += 1
+	if stage > 3 {
+		stage = 1
+	}
+
+	ResponseCookie := http.Cookie{
+		Name:   "stage",
+		Value:  strconv.Itoa(stage),
+		MaxAge: 3600,
+	}
+
+	cookies.WriteCookies(ResponseCookie, w)
+	cb.WriteToUser(Request.WppUser, FinalBody, w)
 
 	return nil
 }
